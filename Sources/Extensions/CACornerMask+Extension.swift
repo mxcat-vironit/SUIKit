569bcc313e3e344a80f4739b2fb471051fb4436d
Maxim Krouk <maximkrouk@gmail.com>
2019-07-18 13:24:49 +0300

    Develop: CACornerMask extension added.

---
+++ Sources/Extensions/CACornerMask+Extension.swift
@@ -0,0 +1,69 @@
+//
+//  CACornerMask+Extension.swift
+//  Pods-SUIKit_Example
+//
+//  Created by Maxim on 7/17/19.
+//
+
+import UIKit
+
+public extension CACornerMask {
+    typealias Corners = Set<Corner>
+    
+    enum Corner: UInt {
+        case topLeft = 1
+        case topRight = 2
+        case bottomLeft = 4
+        case bottomRight = 8
+    }
+    
+}
+
+public extension Array where Element == CACornerMask.Corner {
+    
+    var makeCornerMask: CACornerMask { Set(self).makeCornerMask }
+    
+    var maskValue: UInt { Set(self).maskValue }
+    
+}
+
+public extension Set where Element == CACornerMask.Corner {
+    
+    var makeCornerMask: CACornerMask { CACornerMask(rawValue: maskValue) }
+    
+    var maskValue: UInt {
+        var value: UInt = 0
+        forEach { value += $0.rawValue }
+        return value
+    }
+    
+    static var top: Set<Element> { [.topLeft, .topRight] }
+    
+    static var bottom: Set<Element> { [.bottomLeft, .bottomRight]}
+    
+    static var left: Set<Element> { [.topLeft, .bottomLeft] }
+    
+    static var right: Set<Element> { [.topRight, .bottomRight] }
+    
+    static var tlbr: Set<Element> { [.topLeft, .bottomRight] }
+    
+    static var trbl: Set<Element> { [.topRight, .bottomLeft] }
+    
+    static var all: Set<Element> { [.topLeft, .topRight, .bottomLeft, .bottomRight] }
+    
+    static var none: Set<Element> { [] }
+    
+    static func mask(_ corners: Element...) -> CACornerMask { corners.makeCornerMask }
+    
+    static func mask(_ corners: Set<Element>) -> CACornerMask { corners.makeCornerMask }
+    
+    static func excluding(_ corners: Element...) -> CACornerMask { excluding(Set(corners)) }
+    
+    static func excluding(_ corners: Set<Element>) -> CACornerMask { all.filter{ !corners.contains($0) }.makeCornerMask }
+    
+}
+
+public extension CACornerMask {
+    init(_ corners: Corner...) { self = corners.makeCornerMask }
+    init(_ corners: Corners ) { self = corners.makeCornerMask }
+}
