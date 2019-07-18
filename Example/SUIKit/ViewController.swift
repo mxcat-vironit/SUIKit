c2844edb84ab5f0c04541c403d53900248217a83
Maxim Krouk <maximkrouk@gmail.com>
2019-07-18 14:24:08 +0300

    Project: Example added, iOS version requirements updated.

--- Example/SUIKit/ViewController.swift
+++ Example/SUIKit/ViewController.swift
@@ -6,19 +6,53 @@
 //  Copyright (c) 2019 mx-Cat. All rights reserved.
 //
 
-import UIKit
+import SUIKit
 
 class ViewController: UIViewController {
-
+    
+    var label: UILabel?
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.appendSubview {
+            UIView {
+                UILabel(text: "Hello, SUIKit!", alignment: .center, color: .red)
+                    .frame(view.frame)
+                    .link(to: &label)
+                }
+                .frame(view.frame)
+                .background(color: .black)
+                .cornerRadius(300, corners: .tlbr)
+            }
+            .background(color: .red)
+        
+        hiuser()
     }
-
+    
+    func hiuser() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
+            guard let self = self else { return }
+            self.counter += 1
+            self.label?.text("Hi, User!", color: .yellow)
+            self.view.background(color: .yellow)
+            self.hisuikit()
+        }
+    }
+    
+    func hisuikit() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
+            guard let self = self else { return }
+            self.counter += 1
+            self.label?.text("Hello, SUIKit!", color: .red)
+            self.view.background(color: .red)
+            if self.counter < 10 { self.hiuser() }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
